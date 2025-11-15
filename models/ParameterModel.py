7796f032f5b9bec132603f5412b60af65e293f15
LasseRhys <56411103+LasseRhys@users.noreply.github.com>
2025-11-16 00:31:07 +0100

    Update ParameterModel.py basic

---
+++ models/ParameterModel.py
@@ -0,0 +1,9 @@
+class ParameterModel:
+
+def __init__(self, name: str, type_: str):
+self.name = name
+self.type = type_
+
+
+def __repr__(self):
+return f"ParameterModel(name={self.name}, type={self.type})"
