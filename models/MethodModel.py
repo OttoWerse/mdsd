0e9df98082d1b9f81213a0daabd285a22ef1340f
LasseRhys <56411103+LasseRhys@users.noreply.github.com>
2025-11-16 00:32:06 +0100

    Update MethodModel.py simpel

---
+++ models/MethodModel.py
@@ -0,0 +1,14 @@
+class MethodModel:
+
+def __init__(self, name: str, return_type: str = None):
+self.name = name
+self.return_type = return_type
+self.parameters = [] # List[ParameterModel]
+
+
+def add_parameter(self, parameter):
+self.parameters.append(parameter)
+
+
+def __repr__(self):
+return f"MethodModel(name={self.name}, return_type={self.return_type}, parameters={self.parameters})"
