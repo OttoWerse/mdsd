9acfeaa99fd645a5e531c2e63fc80ca0f60ffd35
Otto Werse <otto.werse@web.de>
2025-12-06 15:32:24 +0100

    Some work on parser

--- parser/XMIParser.py
+++ parser/XMIParser.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+# This is only here to make PyCharm check for this package!
 import lxml
 
 
@@ -10,8 +11,10 @@ class XmiParser:
             file_content = file_object.read()
             # TODO: Check for lxml installed! (required by BS4 but not correctly enforced)
             self.xmi_tree = BeautifulSoup(file_content, 'xml')
+            # TODO: Is this all we really need?
+            self.nodes = self.find_all_elements_by_name('ownedMember')
         except Exception as e:
-            print(f'{e}')
+            print(f'EXCEPTION: {e}')
 
     def find_all_elements_by_name(self, name):
         """Finds elements of a given name in entire tree"""
@@ -22,35 +25,54 @@ class XmiParser:
         # TODO: Check if result is None or nah?
         return result
 
+    def get_all_classes(self):
+        """Returns all classes in the parsers xml tree"""
+        classes = []
+        for node in self.nodes:
+            try:
+                node_type = node['xmi:type']
+                if node_type == 'uml:Class':
+                    # TODO: Create ClassModel Object and append those instead
+                    class_name = node['name']
+                    classes.append(node)
+            except Exception as e:
+                print(f'EXCEPTION: {e}')
+                return []
+        return classes
+
+    def get_all_associations(self):
+        """Returns all associations in the parsers xml tree"""
+        associations = []
+        for node in self.nodes:
+            try:
+                node_type = node['xmi:type']
+                if node_type == 'uml:Association':
+                    # TODO: Create AssociationModel Object and append those instead
+                    try:
+                        association_name = node['name']
+                    except Exception as e:
+                        association_name = node['xmi:type']
+                    ends = self.get_all_ends_of_association(node)
+                    associations.append(node)
+            except Exception as e:
+                print(f'EXCEPTION: {e}')
+                return []
+        return associations
+
+    def get_all_ends_of_association(self, association):
+        """Returns all ends of a given association"""
+        ends = []
+        for child in association.children:
+            match child.name:
+                case 'ownedEnd':
+                    end_id = child['xmi:id']
+                    print(end_id)
+                    ends.append(child)
+        return ends
+
 
 if __name__ == '__main__':
     """Main function"""
     parser = XmiParser(r'H:\Users\Otto\Documents\VPProjects\facade_mikrowelle.xmi')
-    classes = []
-    associations = []
-    nodes = parser.find_all_elements_by_name('ownedMember')
-    for node in nodes:
-        try:
-            # node_namespace = node.namespace
-            # node_name = node.name
-            # node_value = node.value
-            node_type = node['xmi:type']
-            match node_type:
-                case 'uml:Class':
-                    classes.append(node)
-                case 'uml:Association':
-                    associations.append(node)
-        except Exception as e:
-            print(f'{e}')
-    # Handle associations
-    for association in associations:
-        try:
-            association_name = association['name']
-        except Exception as e:
-            try:
-                # TODO: Remove namespace and translate
-                association_name = association['xmi:type']
-            except Exception as e:
-                association_name = 'uses'
-                print(f'{e}')
-        input(association_name)
+    classes = parser.get_all_classes()
+    associations = parser.get_all_associations()
