3c8066049f7834c895024974a2d4c1a459acaf5e
Otto Werse <otto.werse@web.de>
2025-12-23 17:40:14 +0100

    Changed Attribute description to be more textual instead of similar to how Visual Paradigm shows it.

--- templates/German.py
+++ templates/German.py
@@ -8,7 +8,7 @@ Sie hat die folgenden $method_count Methoden:
 $method_list""")
 TYPE_DESCRIPTION = Template("""Typ: $type_name""")
 VISIBILITY_DESCRIPTION = Template("""Sichtbarkeit: $visibility""")
-ATTRIBUTE_DESCRIPTION = Template("""$attribute_visibility $attribute_name : $attribute_type""")
+ATTRIBUTE_DESCRIPTION = Template("""Ein $attribute_visibility Attribut mit dem Namen "$attribute_name" vom Typ $attribute_type""")
 FIELD_DESCRIPTION = Template("""$field_visibility $field_name : $field_type""")
 PARAMETER_DESCRIPTION = Template("""$parameter_type $parameter_name""")
 METHOD_DESCRIPTION = Template("""$method_visibility $method_name($parameters_list) : $return_type""")
