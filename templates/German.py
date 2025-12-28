a6c8d935152dd909f2e4fd8c2b5977dfc7e28d26
LasseRhys <selenfresser@gmx.de>
2025-12-28 19:23:14 +0100

    Templates flüssig

--- templates/German.py
+++ templates/German.py
@@ -1,16 +1,48 @@
 from string import Template
 
 """TEMPLATES"""
-CLASS_DESCRIPTION = Template("""Eine Klasse mit dem Namen $class_name. 
-Sie hat die folgenden $attribute_count Attrbitue: 
-$attribute_list
-Sie hat die folgenden $method_count Methoden: 
-$method_list""")
-TYPE_DESCRIPTION = Template("""Typ: $type_name""")
-VISIBILITY_DESCRIPTION = Template("""Sichtbarkeit: $visibility""")
-ATTRIBUTE_DESCRIPTION = Template("""Ein $attribute_visibility Attribut mit dem Namen "$attribute_name" vom Typ $attribute_type""")
-FIELD_DESCRIPTION = Template("""$field_visibility $field_name : $field_type""")
-PARAMETER_DESCRIPTION = Template("""$parameter_type $parameter_name""")
-METHOD_DESCRIPTION = Template("""$method_visibility $method_name($parameters_list) : $return_type""")
-OPERATION_DESCRIPTION = Template("""$visibility $operation_name($parameters_list) : $return_type""")
-RELATIONSHIP_DESCRIPTION = Template("""Beziehung: $source --[$relation_type]--> $target""")
+CLASS_DESCRIPTION = Template(
+"""Die Klasse "$class_name" repräsentiert eine Einheit im System. 
+Sie besitzt die Attribute: $attribute_list. 
+Zusätzlich stellt sie die folgenden Methoden bereit: $method_list."""
+)
+
+
+TYPE_DESCRIPTION = Template(
+"""Der Typ von "$element_name" ist "$type_name"."""
+)
+
+
+VISIBILITY_DESCRIPTION = Template(
+"""Die Sichtbarkeit von "$element_name" ist "$visibility"."""
+)
+
+ATTRIBUTE_DESCRIPTION = Template(
+"""$attribute_name" vom Typ "$attribute_type" mit Sichtbarkeit "$attribute_visibility"""
+)
+
+FIELD_DESCRIPTION = Template(
+"""Das Feld "$field_name" ist vom Typ "$field_type" und hat die Sichtbarkeit "$field_visibility"."""
+)
+
+
+PARAMETER_DESCRIPTION = Template(
+"""$parameter_name: $parameter_type"""
+)
+
+
+
+METHOD_DESCRIPTION = Template(
+""""$method_name($parameters_list) : $return_type)" ($method_visibility)"""
+)
+
+OPERATION_DESCRIPTION = Template(
+"""Die Operation "$operation_name" ist "$visibility" sichtbar, akzeptiert die Parameter ($parameters_list) und liefert einen Wert vom Typ "$return_type" zurück."""
+)
+
+
+RELATIONSHIP_DESCRIPTION = Template(
+"""Zwischen "$source" und "$target" besteht eine Beziehung vom Typ "$relation_type"."""
+)
+
+
